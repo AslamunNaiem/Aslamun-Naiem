9673a9324cd8ee4ff4e7b039f83ebe8bce391005
AslamunNaiem <95155948+AslamunNaiem@users.noreply.github.com>
2021-11-30 04:43:20 +0000

    update

--- main.dart
+++ main.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
 void main() {    
-    stdout.write("What's your name? ");
-    String name = stdin.readLineSync()!;
-
-    print("Hi, $name! What is your age?");
-    int age = int.parse(stdin.readLineSync()!);
-
-    int yearsToHunderd = 100 - age;
-    print("$name, You have $yearsToHunderd years to be 100");
+    stdout.write("Hi, please choose a number: ");
+    int number = int.parse(stdin.readLineSync()!);
+    
+    if (number % 2 == 0) {
+        print("Chosen number is even");
+    }
+    else {
+        print("Chosen number is odd");
+    }
 }
