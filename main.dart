27658163dfa5264764a2e50954ce0c1d41577f51
AslamunNaiem <95155948+AslamunNaiem@users.noreply.github.com>
2021-11-30 04:53:18 +0000

    update

--- main.dart
+++ main.dart
@@ -1,11 +1,17 @@
-import 'dart:io';
-
 void main() {
-  stdout.write("Please choose a number: ");
-  int number = int.parse(stdin.readLineSync()!);
-  for (var i = 1; i <= number; i++) {
-    if (number % i == 0) {
-      print(i);
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89];
+  Set<int> c = {};
+
+  for (var i in a) {
+    for (var j in b) {
+      if (i == j) {
+        c.add(i);
+      }
     }
   }
+  print(c.toList());
+
+  // One liner using set intersections
+  print(Set.from(a).intersection(Set.from(b)).toList());
 }
